4b7bf755bdfdf88c2b23db03b0c71cbd090e07fd
Martin Burolla <mburolla@xpanxion.com>
2022-06-18 12:36:16 -0400

    update

--- src/Student0/Ex5/Main.js
+++ src/Student0/Ex5/Main.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Main.scss'
 
+import { useState } from 'react'
+
 export const Main = () => {
+
+  let [counter, setCounter] = useState(0)
+
   return (
     <div className="Main">
-       TODO...
+      {counter}
+      <button onClick={() => console.log('here')}>Push</button>
     </div>
   )
 }
