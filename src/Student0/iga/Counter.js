588aedc43de3d7a38798fcfdbe8f10f30930da93
Martin Burolla <mburolla@xpanxion.com>
2022-06-21 12:38:30 -0400

    finish

---
+++ src/Student0/iga/Counter.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useState } from 'react'
+
+import './Counter.scss'
+
+export const Counter = () => {
+    let [counter, setCounter] = useState(0)
+
+    const onHandleClick = (value) => {
+      setCounter(counter = counter + value)
+    }
+  
+    const onHandleReset = () => {
+      setCounter(0)
+    }
+  
+    return (
+      <div>
+          <button className="button" onClick={() => onHandleClick(1)}>Increment by 1</button>
+          <br />
+          <button className="button" onClick={() => onHandleClick(10)}>Increment by 10</button>
+          <br/>
+          <button className="button" onClick={() => onHandleReset()}>Reset</button>
+          <br/>
+          Counter: {counter}
+      </div>
+    )
+}
