c98480e6de1da0b5a50ff30a1424918bf7a492c8
Martin Burolla <mburolla@xpanxion.com>
2022-06-22 07:19:59 -0400

    update

--- src/Student0/lecture/Main.js
+++ src/Student0/lecture/Main.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Counter } from './Counter'
+import { UpDownCounter } from './UpDownCounter'
 
 import './Main.scss'
 
 export const Main = () => {
   return (
     <div>
-      <Counter />
+      <UpDownCounter value={11} />
+      <UpDownCounter value={20} />
+      <UpDownCounter value={30} />
+      <UpDownCounter value="11" />
     </div>
   )
 }
