faca19df25582f173930c8e602119edf92167740
Martin Burolla <mburolla@xpanxion.com>
2022-06-22 11:53:48 -0400

    update

--- src/Student0/API/client.js
+++ src/Student0/API/client.js
@@ -1,12 +1,25 @@
 const axios = require('axios').default;
 
-const getPerson = async () => {
-    let r = await axios.get('http://localhost:5150/person');
+const getPerson = async (userId) => {
+    let r = await axios.get(`http://localhost:5150/person/${userId}`);
+    console.log(r.data);
+}
+
+const addPerson = async (person) => {
+    let r = await axios.post(`http://localhost:5150/person/`, person);
     console.log(r.data);
 }
 
 const main = async () => {
-    getPerson()
+    // let userId = 1
+    // getPerson(userId)
+
+    let p = {
+        id: 3,
+        name: "Alice in Chains",
+        age: 44
+    }
+    addPerson(p)
 }
 
 main();
